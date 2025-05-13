7f6ea4c5e045d4db7b8dd9b490150646fd7dd4e5
Mike Chuang <mchuang3@users.noreply.github.com>
2025-05-13 09:27:39 -0500

    Revert "Sync'd up with Dev repo - ESXi"

--- glm-test-service-image.sh
+++ glm-test-service-image.sh
@@ -77,7 +77,6 @@ echo -e "\nDownload the image from the source:"
 args=(
   --no-proxy #optional parameter
   --no-check-certificate #optional parameter
-  --no-verbose #optional parameter
 )
 DOWNLOAD="wget                                   \
 ${args[*]} `#add parameters from the list above` \
